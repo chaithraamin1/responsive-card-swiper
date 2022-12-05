592cba12a31268970ba30efc89482d386b17cdf7
chai <chaithra.amin98@gmail.com>
2022-12-05 12:17:19 +0530

    swiper js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'responsive-card-swiper';
+  title = 'slider using swiper js';
+  ngOnInit(){
+    debugger
+    var swiper = new Swiper(".slide-container", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      observer: true,
+      observeParents: true,
+      slidesPerGroup: 3,
+      loop: true,
+      // fade:true,
+      grabCursor:true,
+      // centerSlide:true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets:true,
+    
+      },
+      navigation: {
+        nextEl: ".swiper-button-next ",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints:{
+        0:{
+          slidesPerView:1,
+        },
+        520:{
+          slidesPerView:2
+        },
+       768: {
+            slidesPerView:3
+        },
+        1000:{
+          slidesPerView:4
+        }
+      }
+    });
+
+  }
 }
---
+++ src/app/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    observer: true,
+    observeParents: true,
+    slidesPerGroup: 3,
+    loop: true,
+    fade:true,
+    grabCursor:true,
+    centerSlide:true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets:true,
+
+    },
+    navigation: {
+      nextEl: ".swiper-button-next ",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+      0:{
+        slidesPerView:1,
+      },
+      520:{
+        slidesPerView:2
+      },
+     768: {
+          slidesPerView:3
+      },
+      1000:{
+        slidesPerView:4
+      }
+    }
+  });
